1497f40b53ffd35b312e831cffa75c97eecb9614
Matt Schneeberger <helluvamatt@gmail.com>
2016-12-27 01:34:45 -0600

    Add shell for element editor

---
+++ ElementalEditor/ElementEditor.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Elemental.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElementalEditor
+{
+	/// <summary>
+	/// Interaction logic for ElementEditor.xaml
+	/// </summary>
+	public partial class ElementEditor : Window
+	{
+		public Element Element { get; private set; }
+
+		public ElementEditor(Element element)
+		{
+			Element = element;
+			InitializeComponent();
+		}
+
+
+	}
+}
